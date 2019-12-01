d611ee587f8dd5fc8e57e3ce12541c5b6f3b602c
David <davidmedin03@gmail.com>
2019-12-01 10:08:45 -0600

    Finished Level transition - working on tile optomization

--- Object.lua
+++ Object.lua
@@ -4,11 +4,55 @@ function love.resize(w,h)
     WINDOW_X,WINDOW_Y = love.graphics.getDimensions()
 end
 
-local GROUP_TILE = 1
-local GROUP_WALKABLE = 2
-local GROUP_ACTOR = 3
-GLOBAL_ITER = 4
+camX=0
+camY=0
+
+local CATA_TILE = 1
+local CATA_WALKABLE = 2
+local CATA_ACTOR = 3
+local GROUP_ALL = -1
+local GROUP_NONE = 0
 activeWorld = world
+activeMap = nil
+renderables = {activeMap=nil,actors={}}
+mapList = {}
+
+function NormalizeTable(o)
+
+end
+
+function AddMap(map)
+    mapList[#mapList+1] = map
+end
+function GetLoaded()
+    -- count=0
+    -- theOne=nil
+    -- for k,v in pairs(mapList) do
+    --     if v.isLoaded then
+    --         count=count+1
+    --         theOne=v
+    --     end
+    -- end
+    -- if count == 1 then
+    --     return theOne
+    -- end
+    loaded={}
+    count=1
+    for i,v in ipairs(mapList) do
+        if v.loaded then
+            loaded[count] = v
+            count = count+1
+        end
+    end
+    return loaded
+end
+function AddActorRenderables(o)
+    if o.name ~= nil then
+        renderables.actors[o.name]=o
+    else
+        print("no name field in AddActorRenderables table")
+    end
+end
 
 function AddPhysics(o,bodyMode)
     o = o or {}
@@ -27,7 +71,13 @@ function AddPhysics(o,bodyMode)
     end
     return o
 end
-
+--x,y,width,and height are in tiles
+function NewPhysicsObject(x,y,width,height,type,world)
+    object = {}
+    object.body = love.physics.newBody(world,x*TILESIZE,y*TILESIZE,type)
+    object.shape = love.physics.newPolygonShape(0,0,width*TILESIZE,0,width*TILESIZE,height*TILESIZE,0,height*TILESIZE,0,0)
+    object.fixture = love.physics.newFixtureture(object.body,object.shape,1)
+end
 _class = {}
 function _class:new(o)
     o = o or {}
@@ -54,10 +104,19 @@ Tile = _class:new({
     collisionOffsetY=0,
     walkable = true,
     draw = function(self)
-        self.x = self.body:getX()
-        self.y = self.body:getY()
+        self.x = self.body~=nil and self.body:getX()/TILESIZE or self.x
+        self.y = self.body~=nil and self.body:getY()/TILESIZE or self.y
         CentX,CentY = activeMap:CenterScreen()
-        love.graphics.draw(self.image,self.x*activeMap.scale+CentX,self.y*activeMap.scale+CentY,0,self.scale*activeMap.scale)
+        love.graphics.draw(self.image,self.x*TILESIZE*activeMap.scale+CentX,self.y*TILESIZE*activeMap.scale+CentY,0,self.scale*activeMap.scale)
+    end,
+    Enable = function(self,enable)
+        if self.fixture~=nil then
+            if enable then
+                self.fixture:setGroupIndex(GROUP_NONE)
+            else 
+                self.fixture:setGroupIndex(GROUP_ALL)
+            end
+        end
     end,
     new = function(self,o)
         o = o or {}
@@ -68,14 +127,15 @@ Tile = _class:new({
             o.texWidth = o.imgWidth
             o.texHeight = o.imgHeight
         end
-        o = AddPhysics(o,"static")
-        if o.world ~= nil then
-            o.fixture:setCategory(GROUP_TILE)
-            if o.walkable then 
-                o.fixture:setCategory(GROUP_TILE,GROUP_WALKABLE)
-                o.fixture:setMask(GROUP_ACTOR)
-            end
-        end
+        -- o = AddPhysics(o,"static")
+        -- if o.world ~= nil then
+        --     o.fixture:setCategory(CATA_TILE)
+        --     o.fixture:setGroupIndex(GROUP_ALL)
+        --     if o.walkable then 
+        --         o.fixture:setCategory(CATA_TILE,CATA_WALKABLE)
+        --         o.fixture:setMask(CATA_ACTOR)
+        --     end
+        -- end
         return o
         -- renderables[o.name] = o.name
     end
@@ -119,8 +179,12 @@ Actor = _class:new({
         love.graphics:getDimensions(o.image)
         o.imgWidth,o.imgHeight = o.image.getDimensions(o.image)
         o = AddPhysics(o,"dynamic")
-        o.fixture:setCategory(GROUP_ACTOR)
-        o.fixture:setMask(GROUP_WALKABLE)
+        o.sensorShape = love.physics.newPolygonShape(o.shape:getPoints())
+        o.sensor = love.physics.newFixture(o.body,o.sensorShape,1)
+        o.sensor:setSensor(true)
+        o.fixture:setCategory(CATA_ACTOR)
+        o.fixture:setMask(CATA_WALKABLE)
+        o.fixture:setGroupIndex(GROUP_ALL)
         return o
     end
 })
@@ -141,8 +205,19 @@ _Map = _class:new({
     path=nil,
     image=nil, --the image to be loaded to read
     imageData=nil, --the MapImage's Data
-    tileList={}, --list of Tiles
+    tileList=nil, --list of Tiles
+    finalX=0,
+    finalY=0,
+    body=nil,
+    fixture=nil,
+    shape=nil,
+    realitiveY=nil, --is a map
+    realitiveX=nil, --is a map
+    modifyX=nil, --is a string
+    modifyY=nil, --is a string
+    loaded=false,
     FormatedScale = function(self)
+        local WINDOW_X,WINDOW_Y = love.window.getMode()
         local setScale = 1
         if self.imgHeight >= self.imgWidth then
             setScale = WINDOW_Y/(self.imgHeight*8)
@@ -165,33 +240,100 @@ _Map = _class:new({
         self.imgWidth,self.imgHeight = self.image:getDimensions()
         for x=0,self.imgWidth-1 do
             for y=0,self.imgHeight-1 do
-                -- self.tileList[x][y].scale = self.scale
-                 self.tileList[x][y]:draw(1)
+
+                -- love.graphics.setColor(1,1,1)
+                -- if pleb.body:isTouching(self.tileList[x][y].body) then
+                --     love.graphics.setColor(1,0,0)
+                -- end
+
+                self.tileList[x][y]:draw(1)
              end
         end
     end,
-    GetScale = function(self)
-        return self.scale
+    SetActive = function(self,active)
+        if self.loaded == true then
+            if activeMap==self then return end
+            if activeMap ~= nil then
+                for x=0,activeMap.imgWidth-1 do
+                    for y=0,activeMap.imgHeight-1 do
+                        activeMap.tileList[x][y]:Enable(false)
+                    end
+                end
+            end
+            for x=0,self.imgWidth-1 do
+                for y=0,self.imgHeight-1 do
+                    self.tileList[x][y]:Enable(true)
+                end
+            end
+            activeMap=self
+            renderables.activeMap = self
+            camX=-self.finalX*self:FormatedScale()
+            camY=-self.finalY*self:FormatedScale()
+            -- camX=-self.finalX
+            -- camY=-self.finalY
+        else
+            print("Don't try to activate a map before loading it!!")
+        end
+    end,
+    SetAwake = function(self,awake)
+        self.imgWidth,self.imgHeight = self.image:getDimensions()
+        for x=0,self.imgWidth-1 do
+            for y=0,self.imgHeight-1 do
+                self.tileList[x][y].body:setAwake(awake)
+            end
+        end
+    end,
+    load = function(self,toggle)
+        if toggle then
+            if self.loaded == true then
+                -- print("map already loaded!")
+            else
+                self.imageData = love.image.newImageData(self.path)
+                for x=0,self.imgWidth-1 do
+                    self.tileList[x] = {}
+                    for y=0,self.imgHeight-1 do
+                        local r,g,b,a = self.imageData:getPixel(x,y)
+                        for i,v in ipairs(self.colorTranslate) do
+                            if v[1]==r and v[2]==g and v[3]==b and v[4]==a then
+                                self.tileList[x][y] = self.colorTranslate[i].tile:new({x=x+(self.finalX/TILESIZE),y=y+(self.finalY/TILESIZE),world=self.world})
+                                self.scale = self:FormatedScale()
+                            end
+                        end
+                    end
+                end
+            end
+            self.loaded = true
+        else
+            self.loaded=false
+        end
+    end,
+    OptimizeTile = function(self)
+
     end,
     new = function(self,o)
         o = o or {}
+        o.tileList={}
         o = _class.new(self,o) --was Map.parent.new
         o.image = love.graphics.newImage(o.path)
         o.imgWidth,o.imgHeight = o.image:getDimensions()
         -- o.imgWidth,o.imgHeight = o.image.getDimensions(o.image)
-        o.imageData = love.image.newImageData(o.path)
-        for x=0,o.imgWidth-1 do
-            o.tileList[x] = {}
-            for y=0,o.imgHeight-1 do
-                local r,g,b,a = o.imageData:getPixel(x,y)
-                for i,v in ipairs(o.colorTranslate) do
-                    if v[1]==r and v[2]==g and v[3]==b and v[4]==a then
-                        o.tileList[x][y] = o.colorTranslate[i].tile:new({x=x,y=y,world=o.world})
-                        o.scale = o:FormatedScale()
-                    end
-                end
-            end
-        end
+        
+        local realitiveX=0
+        local realitiveY=0
+        if o.realitiveX~=nil then realitiveX=o.realitiveX.x end
+        if o.realitiveY~=nil then realitiveY=o.realitiveY.y end
+        local applyModifyX=0
+        local applyModifyY=0
+        if o.modifyX ~= nil and string.find(o.modifyX,"width")~=nil then applyModifyX=o.imgWidth*TILESIZE if string.find(o.modifyX,"-")~=nil then applyModifyX=applyModifyX*-1 end end
+        if o.modifyY ~= nil and string.find(o.modifyY,"height")~=nil then applyModifyY=o.imgHeight*TILESIZE if string.find(o.modifyY,"-")~=nil then applyModifyY=applyModifyY*-1 end end
+        o.body = love.physics.newBody(o.world,o.x+applyModifyX+realitiveX,o.y+applyModifyY+realitiveY,"static")
+        o.shape=love.physics.newRectangleShape((o.imgWidth*TILESIZE)/2,(o.imgHeight*TILESIZE)/2,o.imgWidth*TILESIZE,o.imgHeight*TILESIZE)
+        o.fixture = love.physics.newFixture(o.body,o.shape,1)
+        o.fixture:setSensor(true)
+        o.finalX=o.x+applyModifyX+realitiveX
+        o.finalY=o.y+applyModifyY+realitiveY
+
+       AddMap(o)
     return o
     end
 })
--- main.lua
+++ main.lua
@@ -1,39 +1,48 @@
 require("Object")
 
-local camX = 0
-local camY = 0
 
-renderables = {}
 
 function love.load()
     love.graphics.setPointSize(5)
     love.physics.setMeter(1)
     love.window.setMode(800,500,{resizable=true})
     love.graphics.setDefaultFilter("nearest")
+    world = love.physics.newWorld(0,0,true);
 
-    world = love.physics.newWorld(0,0,false);
-    _stone = Tile:new({path = "Data/StoneBrick.png",scale=1})
-    _grass = Tile:new({path="Data/Dirt.png",walkable=false,scale=1})
-    map = _Map:new({path = "Data/RGBMap.png",world=world,colorTranslate=
-        {{[1] = 0,[2] = 0,[3] = 0,[4]=1,tile=_stone},
-        {[1]=1,[2]=0,[3]=0,[4]=1,tile=_grass}}
-})
-    activeMap = map
-    pleb = Actor:new({speed = 100,path = "Data/Guard.png",name = "pleb",texWidth = 26,x=1,y=1,world=world,collisionOffsetX=-9,scale=1})
-    renderables.pleb = pleb
-    renderables.map = map
+
+    _stone = Tile:new({path = "Data/StoneBrick.png",scale=1,name="_stone"})
+    _grass = Tile:new({path="Data/Dirt.png",walkable=false,scale=1,name="_grass"})
+    colorTranslate = {{[1] = 0,[2] = 0,[3] = 0,[4]=1,tile=_stone},
+                     {[1]=1,[2]=0,[3]=0,[4]=1,tile=_grass}}
+                     
+    -- map = _Map:new({name="map",path = "Data/RGBMap.png",world=world,colorTranslate=colorTranslate,x=0,y=0})
+    -- level1 = _Map:new({name="level1",path="Data/level-1.png",world=world,colorTranslate=colorTranslate,realitiveY=map,modifyY="-height",y=0})
+    map = _Map:new({name="Rooms",path="Data/Rooms.png",world=world,colorTranslate=colorTranslate,x=0,y=0})
+    map:load(true)
+    map:SetActive(true)
+    pleb = Actor:new({speed = 100,path = "Data/Guard.png",name = "pleb",texWidth = 26,x=6,y=1,world=world,collisionOffsetX=-9,scale=1})
+    AddActorRenderables(pleb)
 end
 
 
 function love.update(dt)
     world:update(dt)
-
-
+    for k,v in pairs(mapList) do
+        if pleb.body:isTouching(v.body) then
+            v:load(true)
+        else
+            v:load(false)
+        end
+    end
+    
+    if #GetLoaded() == 1 then
+        GetLoaded()[1]:SetActive(true)
+    end
     local x,y = 0,0
-   if love.keyboard.isDown("a") then
+    if love.keyboard.isDown("a") then
         x = x + -pleb.speed
     end
-   if love.keyboard.isDown("d") then
+    if love.keyboard.isDown("d") then
         x = x + pleb.speed
     end
     if love.keyboard.isDown("w") then
@@ -55,20 +64,36 @@ end
 
 
 function love.draw()
-    love.graphics.setColor(1,1,1)
     love.graphics.translate(camX,camY)
-    for k,i in pairs(renderables) do
+    love.graphics.setColor(1,1,1)
+    if renderables.activeMap ~= nil then
+        renderables.activeMap:draw(1)
+    end
+    for k,i in pairs(renderables.actors) do
         i:draw(1)
     end
+    
+    
+    -- for _,body in pairs(world:getBodies()) do
+    --     for _,fixture in pairs(body:getFixtures()) do
+    --         local shape = fixture:getShape()
+    --         love.graphics.polygon("line",body:getWorldPoints(shape:getPoints()))
+    --     end
+    -- end
+    -- love.graphics.setColor(1,0,0)
+    -- love.graphics.points(pleb.body:getX(),pleb.body:getY())
+    -- love.graphics.setColor(1,1,1)
+    
+    
+    -- love.graphics.translate(camX,camY)
+    -- love.graphics.setColor(1,0,0)
+    -- if pleb.body:isTouching(map.body) then love.graphics.setColor(1,1,1) end
+    -- love.graphics.polygon("line",map.body:getWorldPoints(map.shape:getPoints()))
+    -- love.graphics.polygon("line",pleb.body:getWorldPoints(pleb.shape:getPoints()))
+    -- love.graphics.setColor(1,0,1)
+    -- if pleb.body:isTouching(level1.body) then love.graphics.setColor(1,1,1) end
 
-    for _,body in pairs(world:getBodies()) do
-        for _,fixture in pairs(body:getFixtures()) do
-            local shape = fixture:getShape()
-            love.graphics.polygon("line",body:getWorldPoints(shape:getPoints()))
-        end
-    end
-    love.graphics.setColor(1,0,0)
-    love.graphics.points(pleb.body:getX(),pleb.body:getY())
-    love.graphics.setColor(1,1,1)
-
+    -- love.graphics.polygon("line",level1.body:getWorldPoints(level1.shape:getPoints()))
+    -- love.graphics.setColor(1,1,1)
+    -- love.graphics.translate(-camX,-camY)
 end
